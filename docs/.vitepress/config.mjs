02701c5bcf60773fe7f275b889776a8eb132f878
Luke Zeches <zechesl@gmail.com>
2024-11-12 10:54:37 -0600

    fix: correct the base URL

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "DICOM Capacitor Documentation",
   description: "Documentation for DICOM Capacitor",
-  base: '/docs/',
+  base: '/dicom-capacitor-docs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
